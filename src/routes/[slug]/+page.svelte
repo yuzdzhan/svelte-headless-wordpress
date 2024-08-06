9eaf5bb9ac73916b4546afaa2fd7a3a44d788fa7
Yuzdzhan Mehmedov <ynmehmedov@gmail.com>
2024-08-06 23:59:03 +0300

    fixes

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -1,13 +1,19 @@
 <script>
     import PostDate from '$lib/components/PostDate.svelte';
     import TableOfContents from '$lib/components/TableOfContents.svelte';
-    import {onMount} from 'svelte';
+    import {afterUpdate, onDestroy, onMount} from 'svelte';
     import {browser} from '$app/environment';
+    import {afterNavigate} from '$app/navigation';
 
     onMount(() => {
         if (browser) {
+            if (window.EnlighterJS) {
+                return;
+            }
+
             const script = document.createElement('script');
             script.src = '/js/enlighter/enlighterjs.min.js';
+            script.id = 'enlighterjs'
             script.onload = () => {
                 // Initialize Enlighter.js
                 window.EnlighterJS.init('pre', 'code', {
@@ -15,6 +21,17 @@
                 });
             };
             document.body.appendChild(script);
+        }
+    });
+
+    afterUpdate(() => {
+        if (browser) {
+            document.querySelectorAll('.enlighter-default').forEach(e => e.remove());
+            if (window.EnlighterJS) {
+                window.EnlighterJS.init('pre', 'code', {
+                    indent: 2
+                });
+            }
         }
     });
 
@@ -35,7 +52,7 @@
 <PostDate date={date}/>
 
 <article class="post">
-    <TableOfContents toc={data.tableOfContents} />
+    <TableOfContents toc={data.tableOfContents}/>
     {@html data.post.content.rendered}
 </article>
 
@@ -46,6 +63,7 @@
         font-weight: 600;
         color: rgba(var(--color-accent));
     }
+
     article {
         margin-top: 2rem;
     }
