b0763b9f442d9f196043ebb7a0867cd17a76fd81
Yuzdzhan Mehmedov <ynmehmedov@superhosting.bg>
2024-08-23 16:14:27 +0300

    Correction

--- src/lib/wordpressAPI.js
+++ src/lib/wordpressAPI.js
@@ -32,7 +32,6 @@ class WordPressAPI {
    */
   async #getPaginated(endpoint, params = {}) {
     const url = new URL(`${this.baseUrl}/${endpoint}`);
-    console.log(url)
     Object.entries(params).forEach(([key, value]) =>
       url.searchParams.append(key, value.toString())
     );
